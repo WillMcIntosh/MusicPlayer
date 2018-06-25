5d6dcd7648cd65b600e47335aabf21709079a799
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-06-24 20:55:24 -0400

    stopping place for the night. need to finish passing intent extras.

---
+++ app/src/main/java/com/mcintosh/musicplayer/PlayingActivity.java
@@ -0,0 +1,20 @@
+package com.mcintosh.musicplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class PlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.now_playing);
+
+        // TODO: need to receive the intent extras and set text of views accordingly
+
+
+    }
+}
