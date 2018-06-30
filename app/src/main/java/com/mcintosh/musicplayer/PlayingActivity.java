c44560b00c8a58fa6bd1f2575af78a68f2e0dcdd
WillMcIntosh <mcintoshwilliamh@gmail.com>
2018-06-30 17:54:35 -0400

    fixed app in landscape mode and added ids for textviews to update

--- app/src/main/java/com/mcintosh/musicplayer/PlayingActivity.java
+++ app/src/main/java/com/mcintosh/musicplayer/PlayingActivity.java
@@ -3,6 +3,7 @@ package com.mcintosh.musicplayer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,23 +16,27 @@ public class PlayingActivity extends AppCompatActivity {
         setContentView(R.layout.now_playing);
 
         // get info selected from Intent on Library page
-        final String selectedTitle = getIntent().getStringExtra("TITLE");
-        final String selectedArtist = getIntent().getStringExtra("ARTIST");
-        final String selectedAlbum = getIntent().getStringExtra("ALBUM");
-        final String selectedArtwork = getIntent().getStringExtra("ART");
-        // Song expects an int value for artwork location
-        int artIntValue = Integer.parseInt(selectedArtwork);
 
+        Bundle extras = getIntent().getExtras();
+        String selectedTitle = "No Title Selected";
+        String selectedArtist = "No Artist Selected";
+        String selectedAlbum = "No Album Selected";
+        Integer selectedArtwork = 0;
+
+        if (extras != null) {
+            selectedTitle = extras.getString("TITLE");
+            selectedArtist = extras.getString("ARTIST");
+            selectedAlbum = extras.getString("ALBUM");
+            selectedArtwork = extras.getInt("ART");
+        }
+
+
+        Log.v("ARTWORK LOCATION IS AT", "" + selectedArtwork);
         // create Song object out of passed info
-        Song currentSong = new Song(selectedTitle, selectedArtist, selectedAlbum, artIntValue);
+        Song currentSong = new Song(selectedTitle, selectedArtist, selectedAlbum, selectedArtwork);
 
         // TODO: set values of layout based on Song currentSong
-//        SongAdapter adapter =
-//                new SongAdapter(this, currentSong);
-//
-//        ListView listView = (ListView) findViewById(R.id.list);
-//
-//        listView.setAdapter(adapter);
+
 
 
 
